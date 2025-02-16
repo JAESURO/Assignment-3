7d7d9a1b7291178657e596867bbaeffb9a6f99ac
JAESURO <amantaiulyzhandos2005@gmail.com>
2025-02-16 06:22:25 +0500

    Updated .env

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ const app = express();
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('MongoDB connection error:', err));
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected successfully!"))
+.catch(err => console.error("MongoDB connection error:", err));
 
 const productSchema = new mongoose.Schema({
   name: String,
